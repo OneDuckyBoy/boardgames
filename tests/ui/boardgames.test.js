92ded4f0ad133c705cfc25c23afbff471a71745a
OneDuckyBoy <stilianmatev@gmail.com>
2024-03-27 23:17:08 +0200

    add UI tests to the cd pipeline

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('http://localhost:8080/boardgames');
+    await page.goto('https://stilianmatev-exam-prep-for-devops-course.onrender.com/boardgames');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
